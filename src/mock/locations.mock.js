971d7f1221ebd498efb1c58bb72d97d36ffdff16
Matheus Rocha <matheus_rocha98@outlook.com>
2020-06-09 20:36:00 -0300

    chore: creating a mock for locations (#6 JSON in README.md)

---
+++ src/mock/locations.mock.js
@@ -0,0 +1,80 @@
+// deixei apenas as informações que serão utilizadas
+export default [
+  {
+    refe_codigo: 2469933,
+    refe_descricao: 'teste',
+    refe_endereco_empresa_terceiro: 'RUA ITAPIRU',
+    cida_cidade: {
+      cida_descricao: 'SAO PAULO',
+      esta_estado: {
+        esta_sigla: 'SP',
+      },
+    },
+  },
+  {
+    refe_codigo: 2032730,
+    refe_descricao: 'FAMA',
+    refe_endereco_empresa_terceiro: 'AVENIDA CONTORNO SUL',
+    cida_cidade: {
+      cida_descricao: 'APUCARANA',
+      esta_estado: {
+        esta_sigla: 'PR',
+      },
+    },
+  },
+  {
+    refe_codigo: 2032716,
+    refe_descricao: 'KYB KAYABA DO BRASIL',
+    refe_endereco_empresa_terceiro: 'AVENIDA FRANCISCO FERREIRA DA CRUZ',
+    cida_cidade: {
+      cida_descricao: 'FAZENDA RIO GRANDE',
+      esta_estado: {
+        esta_sigla: 'PR',
+      },
+    },
+  },
+  {
+    refe_codigo: 2096146,
+    refe_descricao: 'MARSON COMERCIO E CONSTRUÇÃO',
+    refe_endereco_empresa_terceiro: 'AVENIDA ARIOVALDO VIANA',
+    cida_cidade: {
+      cida_descricao: 'SERRA NEGRA',
+      esta_estado: {
+        esta_sigla: 'SP',
+      },
+    },
+  },
+  {
+    refe_codigo: 2031424,
+    refe_descricao: 'AUTOMOLAS EQUIPAMENTOS LTDA',
+    refe_endereco_empresa_terceiro: 'RODOVIA MELLO PEIXOTO',
+    cida_cidade: {
+      cida_descricao: 'CAMBE',
+      esta_estado: {
+        esta_sigla: 'PR',
+      },
+    },
+  },
+  {
+    refe_codigo: 2031412,
+    refe_descricao: 'GERDAU S.A - PIN',
+    refe_endereco_empresa_terceiro: 'AVENIDA ENGENHEIRO LUIZ DUMONT VILLARES',
+    cida_cidade: {
+      cida_descricao: 'PINDAMONHANGABA',
+      esta_estado: {
+        esta_sigla: 'SP',
+      },
+    },
+  },
+  {
+    refe_codigo: 2032713,
+    refe_descricao: 'CINPAL CIA INDUSTRIA DE PEÇAS',
+    refe_endereco_empresa_terceiro: 'AVENIDA PAULO AYRES',
+    cida_cidade: {
+      cida_descricao: 'TABOAO DA SERRA',
+      esta_estado: {
+        esta_sigla: 'SP',
+      },
+    },
+  },
+];
