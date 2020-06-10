25dc5c511b2dad91bba6450e56f223030e8bc196
Matheus Rocha <matheus_rocha98@outlook.com>
2020-06-10 15:44:57 -0300

    feat: finishing step-10

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import Step6 from './pages/step6';
 import Step7 from './pages/step7';
 import Step8 from './pages/step8';
 import Step9 from './pages/step9';
+import Step10 from './pages/step10';
 import NotFound from './pages/notFound';
 
 export default function Routes() {
@@ -23,6 +24,7 @@ export default function Routes() {
       <Route path="/step-7" component={Step7} />
       <Route path="/step-8" component={Step8} />
       <Route path="/step-9" component={Step9} />
+      <Route path="/step-10" component={Step10} />
       <Route path="/" exact render={() => <Redirect to="/step-1" />} />
       <Route path="*" component={NotFound} />
     </Switch>
