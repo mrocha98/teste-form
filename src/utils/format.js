ca17f8540d2818d664e5a63cc45cfbc6ce28d10c
Matheus Rocha <matheus_rocha98@outlook.com>
2020-06-11 01:33:51 -0300

    feat: creating utils functions to serialize data

---
+++ src/utils/format.js
@@ -0,0 +1,13 @@
+import { parseISO } from 'date-fns';
+
+export const formatDate = (date) => parseISO(date).toLocaleDateString('pt-BR');
+
+export const formatMoney = (value) =>
+  new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(value);
+
+export const parseOriginWithMock = (origin, mock) => {
+  const formated = origin.reduce((newArr, item, index) => newArr.concat({ status: item, position: index }), []);
+  const activatedItems = formated.filter((item) => item.status);
+  const parsed = activatedItems.map((item) => mock[item.position]);
+  return parsed;
+};
